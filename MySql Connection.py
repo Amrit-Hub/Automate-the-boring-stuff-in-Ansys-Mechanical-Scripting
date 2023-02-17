812a8101f81f30ae7d150eb6321b5c413315c6fb
Amrit-Hub <itsamrit4u@gmail.com>
2023-02-17 14:57:56 +0530

    added mysql commands

--- MySql Connection.py
+++ MySql Connection.py
@@ -1,3 +1,72 @@
 import clr
-clr.AddReferenceToFile('MySql.Data.dll')
+clr.AddReferenceToFile('MySql.Data.dll') # Namespace
 from MySql.Data import MySqlClient
+
+Server = 'vector'
+Database = 'world'
+User = 'amritsql'
+Password = 'Amrit-sql876'
+connectionString = 'server={}; database={}; uid={}; pwd={}'.format(Server, Database, User, Password)
+connection = MySqlClient.MySqlConnection(connectionString)
+
+query = "select * from world.city"
+
+# cmd = connection.CreateCommand()
+# cmd.CommandText = query
+
+# try:
+#     connection.Open()
+#     reader = cmd.ExecuteReader()
+#     print(reader.HasRows)
+#     print(reader.FieldCount)
+#     while reader.Read():
+#         print(reader['id'])
+#         # print(reader[0])
+#     connection.Close()
+# except Exception as e:
+#     print(e)
+#     connection.Close()
+
+# cmd = MySqlClient.MySqlCommand()
+# cmd.Connection = connection
+# cmd.CommandText = query
+
+# try:
+#     connection.Open()
+#     reader = cmd.ExecuteReader()
+#     print(reader.HasRows)
+#     print(reader.FieldCount)
+#     while reader.Read():
+#         print(reader['id'])
+#         # print(reader[0])
+#     connection.Close()
+# except Exception as e:
+#     print(e)
+#     connection.Close()
+
+cmd = MySqlClient.MySqlCommand(query, connection)
+
+try:
+    connection.Open()
+    reader = cmd.ExecuteReader()
+    print(reader.HasRows)
+    print(reader.FieldCount)
+    while reader.Read():
+        print(reader['id'])
+        # print(reader[0])
+    connection.Close()
+except Exception as e:
+    print(e)
+    connection.Close()
+
+# nonQuery = "insert into dbo.tablea (id) values (5)"
+# # nonQuery = "delete from dbo.tablea where id = 5"
+# cmd = MySqlClient.MySqlCommand(nonQuery, connection)
+
+# try:
+#     connection.Open()
+#     cmd.ExecuteNonQuery()
+#     connection.Close()
+# except Exception as e:
+#     print(e)
+#     connection.Close()
--- SqlServer Connection.py
+++ SqlServer Connection.py
@@ -6,44 +6,44 @@ Server = 'TIGER03957\SQLSERVER2022'
 Database = 'dbapsqlserver'
 User = 'amritsql'
 Password = 'Amrit-sql876'
-connectionString = f'Server = {Server}; Database = {Database}; Integrated Security=True; Connect Timeout=10'
+connectionString = 'Server = {}; Database = {}; Integrated Security=True; Connect Timeout=10'.format(Server, Database)
 connection = SqlClient.SqlConnection(connectionString)
 
 query = "select * from dbo.tablea"
 
-cmd = connection.CreateCommand()
-cmd.CommandText = query
+# cmd = connection.CreateCommand()
+# cmd.CommandText = query
 
-try:
-    connection.Open()
-    reader = cmd.ExecuteReader()
-    print(reader.HasRows)
-    print(reader.FieldCount)
-    while reader.Read():
-        print(reader['id'])
-        # print(reader[0])
-    connection.Close()
-except Exception as e:
-    print(e)
-    connection.Close()
+# try:
+#     connection.Open()
+#     reader = cmd.ExecuteReader()
+#     print(reader.HasRows)
+#     print(reader.FieldCount)
+#     while reader.Read():
+#         print(reader['id'])
+#         # print(reader[0])
+#     connection.Close()
+# except Exception as e:
+#     print(e)
+#     connection.Close()
 
-cmd = SqlClient.SqlCommand()
-cmd.Connection = connection
-cmd.CommandText = query
+# cmd = SqlClient.SqlCommand()
+# cmd.Connection = connection
+# cmd.CommandText = query
 
-try:
-    connection.Open()
-    reader = cmd.ExecuteReader()
-    print(reader.HasRows)
-    print(reader.FieldCount)
-    while reader.Read():
-        print(reader['id'])
-        # print(reader[0])
-    connection.Close()
-except Exception as e:
-    print(e)
-    connection.Close()
-
+# try:
+#     connection.Open()
+#     reader = cmd.ExecuteReader()
+#     print(reader.HasRows)
+#     print(reader.FieldCount)
+#     while reader.Read():
+#         print(reader['id'])
+#         # print(reader[0])
+#     connection.Close()
+# except Exception as e:
+#     print(e)
+#     connection.Close()
+# 
 cmd = SqlClient.SqlCommand(query, connection)
 
 try:
@@ -58,3 +58,15 @@ try:
 except Exception as e:
     print(e)
     connection.Close()
+
+# nonQuery = "insert into dbo.tablea (id) values (5)"
+# # nonQuery = "delete from dbo.tablea where id = 5"
+# cmd = SqlClient.SqlCommand(nonQuery, connection)
+
+# try:
+#     connection.Open()
+#     cmd.ExecuteNonQuery()
+#     connection.Close()
+# except Exception as e:
+#     print(e)
+#     connection.Close()
